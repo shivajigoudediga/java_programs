41e3526a225f1882a2ab9af14b20d6d6dae95ee9
Shivaji <167970486+shivajigoudediga@users.noreply.github.com>
2025-06-13 11:56:32 +0530

    Add files via upload

---
+++ even_odd_from_1toN.java
@@ -0,0 +1,17 @@
+//Print all even and odd numbers from 1 to N – Use if condition inside loop.
+import java.io.*;
+import java.util.*;
+public class even_odd_from_1toN{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+			if(i%2==0){
+				System.out.println("even numbers are:"+i);
+			}
+			else{
+				System.out.println("odd numbers are:"+i);
+			}
+		}
+	}
+}
