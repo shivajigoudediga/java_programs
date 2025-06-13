7e3ec6a9d8cebda1c4f5b157740d58270b6df1d9
Shivaji <167970486+shivajigoudediga@users.noreply.github.com>
2025-06-13 12:06:24 +0530

    sum_of_first_natural_numbers.java

--- sum_of_first_natural_numbers.java
+++ sum_of_first_natural_numbers.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class sum_of_first_natural_numbers{
 	public static void main(String[]args){
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the N number to find sum");
 		int n=sc.nextInt();
 		int k=0;
 		for(int i=0;i<=n;i++){
