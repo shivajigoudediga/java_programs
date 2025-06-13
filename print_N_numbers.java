5041f0678378c843633cf6bba9e3d5adc0bd9ac3
Shivaji <167970486+shivajigoudediga@users.noreply.github.com>
2025-06-13 11:38:49 +0530

    Add files via upload

---
+++ print_N_numbers.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class print_N_numbers{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<=n;i++){
+			System.out.print("N_Numbers are:"+i);
+		}
+	}
+}
