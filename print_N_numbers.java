2770755129a318eb27f396dae4c98de9c4f7a8c5
Shivaji <167970486+shivajigoudediga@users.noreply.github.com>
2025-06-13 11:54:30 +0530

    print_N_numbers.java

--- print_N_numbers.java
+++ print_N_numbers.java
@@ -4,8 +4,13 @@ public class print_N_numbers{
 	public static void main(String[]args){
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		for(int i=0;i<=n;i++){
-			System.out.print("N_Numbers are:"+i);
+		for(int i=1;i<=n;i++){
+			if(i<=1){
+				System.out.println("N_Numbers  from 1 to N are:\n"+i);
+			}
+			else{
+				System.out.println(i);
+			}	
 		}
 	}
 }
